8cb1aa0a0c1c4611a417c075ab666e8023233f18
GAV11N <lemonjack12345@gmail.com>
2015-09-29 09:53:14 -0500

    Runway
    Added 4 LED lights that strobe

--- blinky/blinky.ino
+++ blinky/blinky.ino
@@ -1,16 +1,67 @@
 // Gavin Andrew
 
-#define RED_LED 7
+#define YELLOW_LED 7
+#define RED_LED 6
+#define GREEN_LED 5
+#define CIRCLE_LED 4
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(YELLOW_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(CIRCLE_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(15);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(15);
   digitalWrite(RED_LED, HIGH);
-  delay(1000);
+  delay(15);
   digitalWrite(RED_LED, LOW);
-  delay(1000);
+  delay(15);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(15);
+  digitalWrite(GREEN_LED, LOW);
+  delay(15);
+  digitalWrite(CIRCLE_LED, HIGH);
+  delay(15);
+  digitalWrite(CIRCLE_LED, LOW);
+  delay(15);
+}// Gavin Andrew
+
+#define YELLOW_LED 7
+#define RED_LED 6
+#define GREEN_LED 5
+#define CIRCLE_LED 4
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(CIRCLE_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(15);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(15);
+  digitalWrite(RED_LED, HIGH);
+  delay(15);
+  digitalWrite(RED_LED, LOW);
+  delay(15);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(15);
+  digitalWrite(GREEN_LED, LOW);
+  delay(15);
+  digitalWrite(CIRCLE_LED, HIGH);
+  delay(15);
+  digitalWrite(CIRCLE_LED, LOW);
+  delay(15);
 }
