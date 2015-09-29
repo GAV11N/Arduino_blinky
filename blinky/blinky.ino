d3e7487bab8129b741b9e26c64f3ba78b68e363f
GAV11N <lemonjack12345@gmail.com>
2015-09-29 09:58:57 -0500

    Runway (FIX)
    Made 4 LED lights strobe

--- blinky/blinky.ino
+++ blinky/blinky.ino
@@ -16,52 +16,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(YELLOW_LED, HIGH);
-  delay(15);
+  delay(75);
   digitalWrite(YELLOW_LED, LOW);
-  delay(15);
+  delay(75);
   digitalWrite(RED_LED, HIGH);
-  delay(15);
+  delay(75);
   digitalWrite(RED_LED, LOW);
-  delay(15);
+  delay(75);
   digitalWrite(GREEN_LED, HIGH);
-  delay(15);
+  delay(75);
   digitalWrite(GREEN_LED, LOW);
-  delay(15);
+  delay(75);
   digitalWrite(CIRCLE_LED, HIGH);
-  delay(15);
+  delay(75);
   digitalWrite(CIRCLE_LED, LOW);
-  delay(15);
-}// Gavin Andrew
-
-#define YELLOW_LED 7
-#define RED_LED 6
-#define GREEN_LED 5
-#define CIRCLE_LED 4
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(YELLOW_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(CIRCLE_LED, OUTPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(YELLOW_LED, HIGH);
-  delay(15);
-  digitalWrite(YELLOW_LED, LOW);
-  delay(15);
-  digitalWrite(RED_LED, HIGH);
-  delay(15);
-  digitalWrite(RED_LED, LOW);
-  delay(15);
-  digitalWrite(GREEN_LED, HIGH);
-  delay(15);
-  digitalWrite(GREEN_LED, LOW);
-  delay(15);
-  digitalWrite(CIRCLE_LED, HIGH);
-  delay(15);
-  digitalWrite(CIRCLE_LED, LOW);
-  delay(15);
+  delay(75);
 }
